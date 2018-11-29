c07b7652d92b6c873580c762e086790aacde5418
Nikolas Erkinheimo <nikolas.erkinheimo@aalto.fi>
2018-11-29 12:54:27 +0200

    Added k-means

--- scipy_testing.py
+++ scipy_testing.py
@@ -1,27 +1,19 @@
 import numpy as np
 import scipy.sparse as sparse
+from sklearn.cluster import KMeans
 import pandas as pd
 import networkx as nx
 
-file      = ['ca-AstroPh.txt']
-edgelist  = pd.read_csv(file[0], delimiter='\t', skiprows=4, header=None)
-G         = nx.from_pandas_edgelist(edgelist, source=0, target=1, create_using=nx.DiGraph())
-adj_matr  = nx.to_pandas_adjacency(G, dtype=np.float32)
+path      = 'graphs/'
+file      = ['ca-GrQc.txt']
+k         = 4
 
-"""
-N = len(edgelist[0].unique())
-in_degrees  = {}
-out_degrees = {}
-for i in edgelist[0].unique():
-    in_degrees[i]  = 0
-    out_degrees[i] = 0
-
-for edge in edgelist.values:
-    in_degrees[edge[1]]  += 1
-    out_degrees[edge[0]] += 1
-"""
-
-# seuraavaksi pitäisi kertoa kaikki arvot -1/np.sqrt(out_degree[vi] * out_degree[vj])
+edgelist  = pd.read_csv(path + file[0], delimiter=' ', skiprows=1, header=None)
+G         = nx.from_pandas_edgelist(edgelist, source=0, target=1, create_using=nx.Graph())
+adj_matr  = nx.to_pandas_adjacency(G, dtype=np.float64)
 laplacian = sparse.csgraph.laplacian(adj_matr.values, normed=True)
 
-print(laplacian)
+eigenvalues, eigenvectors = np.linalg.eig(laplacian)
+kmeans = KMeans(n_clusters=k, max_iter=3000).fit(eigenvectors.astype(np.float64))
+
+print(kmeans.labels_)
